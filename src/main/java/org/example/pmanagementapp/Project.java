61c5a47e4ea89c6a0226616d960638e561400f03
erolyesilyurt <erolyes10@gmail.com>
2024-05-16 10:56:14 +0300

    fonksiyonlar

--- src/main/java/org/example/pmanagementapp/Project.java
+++ src/main/java/org/example/pmanagementapp/Project.java
@@ -1,7 +1,31 @@
 package org.example.pmanagementapp;
 
 public class Project {
-    public String projectTitle;
-    public String projectDate;
-    public double projectStage;
+    private String projectTitle;
+    private String projectDate;
+    private double projectStage;
+
+    public double getProjectStage() {
+        return projectStage;
+    }
+
+    public void setProjectStage(double projectStage) {
+        this.projectStage = projectStage;
+    }
+
+    public String getProjectDate() {
+        return projectDate;
+    }
+
+    public void setProjectDate(String projectDate) {
+        this.projectDate = projectDate;
+    }
+
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
+    public void setProjectTitle(String projectTitle) {
+        this.projectTitle = projectTitle;
+    }
 }
